a26ce697ae5392a200361da0a41d014508bc5309
Mischa Lewis-Norelle <mischa.lewis-norelle@appfolio.com>
2017-11-29 16:26:46 -0800

    mln - reorder script run to decrease chance of mistakes
    Because I started with the less restructuve regexes first, sometimes a rule would be applied incorrectly, when a later rule would have made the correct transformation

--- fix_assertions_script.rb
+++ fix_assertions_script.rb
@@ -2,46 +2,22 @@ require 'find'
 require_relative 'fix_assertions'
 
 Find.find('.') do |path|
-  if path =~ /.*\.rb$/
+  if path =~ /.*test\.rb$/
     text = File.read(path)
 
     6.times do
-      # Apply the simple assert_difference calls
-      find = build_simple_assert_difference_find_regex(count_needed: true, parenthesis_needed: false)
-      replace = build_simple_assert_difference_replace_regex(count_needed: true)
-      text = text.gsub(Regexp.new(find), replace)
-
-      find = build_simple_assert_difference_find_regex(count_needed: false, parenthesis_needed: false)
-      replace = build_simple_assert_difference_replace_regex(count_needed: false)
-      text = text.gsub(Regexp.new(find), replace)
-
-      find = build_simple_assert_difference_find_regex(count_needed: true, parenthesis_needed: true)
-      replace = build_simple_assert_difference_replace_regex(count_needed: true)
-      text = text.gsub(Regexp.new(find), replace)
-
-      find = build_simple_assert_difference_find_regex(count_needed: false, parenthesis_needed: true)
-      replace = build_simple_assert_difference_replace_regex(count_needed: false)
-      text = text.gsub(Regexp.new(find), replace)
-
-      find = build_simple_assert_no_difference_find_regex(parenthesis_needed: false)
-      replace = build_simple_assert_no_difference_replace_regex
-      text = text.gsub(Regexp.new(find), replace)
-
-      find = build_simple_assert_no_difference_find_regex(parenthesis_needed: true)
-      replace = build_simple_assert_no_difference_replace_regex
-      text = text.gsub(Regexp.new(find), replace)
-
-      10.times do |time|
+      # Apply the complex assert_difference calls
+      10.downto(1) do |time|
         find = build_complex_assert_difference_find_regex(number_of_classes: time + 1, count_needed: true, parenthesis_needed: false)
+        replace = build_complex_assert_difference_replace_regex(number_of_classes: time + 1, count_needed: true)
+        text = text.gsub(Regexp.new(find), replace)
+
+        find = build_complex_assert_difference_find_regex(number_of_classes: time + 1, count_needed: true, parenthesis_needed: true)
         replace = build_complex_assert_difference_replace_regex(number_of_classes: time + 1, count_needed: true)
         text = text.gsub(Regexp.new(find), replace)
 
         find = build_complex_assert_difference_find_regex(number_of_classes: time + 1, count_needed: false, parenthesis_needed: false)
         replace = build_complex_assert_difference_replace_regex(number_of_classes: time + 1, count_needed: false)
-        text = text.gsub(Regexp.new(find), replace)
-
-        find = build_complex_assert_difference_find_regex(number_of_classes: time + 1, count_needed: true, parenthesis_needed: true)
-        replace = build_complex_assert_difference_replace_regex(number_of_classes: time + 1, count_needed: true)
         text = text.gsub(Regexp.new(find), replace)
 
         find = build_complex_assert_difference_find_regex(number_of_classes: time + 1, count_needed: false, parenthesis_needed: true)
@@ -56,6 +32,31 @@ Find.find('.') do |path|
         replace = build_complex_assert_no_difference_replace_regex(number_of_classes: time + 1)
         text = text.gsub(Regexp.new(find), replace)
       end
+
+      # Apply the simple assert_difference calls
+      find = build_simple_assert_difference_find_regex(count_needed: true, parenthesis_needed: false)
+      replace = build_simple_assert_difference_replace_regex(count_needed: true)
+      text = text.gsub(Regexp.new(find), replace)
+
+      find = build_simple_assert_difference_find_regex(count_needed: true, parenthesis_needed: true)
+      replace = build_simple_assert_difference_replace_regex(count_needed: true)
+      text = text.gsub(Regexp.new(find), replace)
+
+      find = build_simple_assert_difference_find_regex(count_needed: false, parenthesis_needed: false)
+      replace = build_simple_assert_difference_replace_regex(count_needed: false)
+      text = text.gsub(Regexp.new(find), replace)
+
+      find = build_simple_assert_difference_find_regex(count_needed: false, parenthesis_needed: true)
+      replace = build_simple_assert_difference_replace_regex(count_needed: false)
+      text = text.gsub(Regexp.new(find), replace)
+
+      find = build_simple_assert_no_difference_find_regex(parenthesis_needed: false)
+      replace = build_simple_assert_no_difference_replace_regex
+      text = text.gsub(Regexp.new(find), replace)
+
+      find = build_simple_assert_no_difference_find_regex(parenthesis_needed: true)
+      replace = build_simple_assert_no_difference_replace_regex
+      text = text.gsub(Regexp.new(find), replace)
     end
 
     File.open(path, "w") {|file| file.puts text }
